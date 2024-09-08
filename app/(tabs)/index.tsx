d07f10e36705b0c20da6ecf5f1572bdc86ea2f53
adelpro <adelpro@gmail.com>
2024-09-08 18:19:29 +0100

    feat(home): mak ehte mushaf work on the home page with params

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { Image } from 'expo-image';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import {
+  PanGestureHandler,
+  PanGestureHandlerStateChangeEvent,
+  State,
+} from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSetRecoilState } from 'recoil';
 
@@ -8,54 +15,90 @@ import { ThemedView } from '@/components/ThemedView';
 import TopMenu from '@/components/TopMenu';
 import { topMenuState } from '@/recoil/atoms';
 
+import specs from '../../assets/quran-metadata/mushaf-elmadina-warsh-azrak/specs.json';
+
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function HomeScreen() {
   const setShowTopMenu = useSetRecoilState(topMenuState);
+  const router = useRouter();
+  const { page: pageParam } = useLocalSearchParams();
+  const defaultNumberOfPages = specs.defaultNumberOfPages;
+  const currentPage =
+    typeof pageParam === 'string' ? parseInt(pageParam, 10) : 1;
+  const [imageSrc, setImageSrc] = useState<string>(
+    pageParam
+      ? `/assets/mushaf-data/mushaf-elmadina-warsh-azrak/${pageParam}.png`
+      : `/assets/mushaf-data/mushaf-elmadina-warsh-azrak/1.png`,
+  );
+
+  const handleSwipe = (event: PanGestureHandlerStateChangeEvent) => {
+    const { nativeEvent } = event;
+    if (nativeEvent.state === State.END) {
+      if (nativeEvent.translationX > 50) {
+        // Swipe Right - Go to the previous page
+        let page = currentPage + 1;
+        if (page > defaultNumberOfPages) {
+          page = defaultNumberOfPages;
+        }
+        console.log('swipe right to:', page, currentPage);
+        setImageSrc(
+          `/assets/mushaf-data/mushaf-elmadina-warsh-azrak/${page}.png`,
+        );
+        router.replace({
+          pathname: '/(tabs)',
+          params: { page: page.toString() },
+        });
+      } else if (nativeEvent.translationX < -50) {
+        // Swipe Left - Go to the next page
+        let page = currentPage - 1;
+        if (page < 1) {
+          page = 1;
+        }
+        console.log('swipe left to:', page);
+        setImageSrc(
+          `/assets/mushaf-data/mushaf-elmadina-warsh-azrak/${page}.png`,
+        );
+        router.replace({
+          pathname: '/(tabs)',
+          params: { page: page.toString() },
+        });
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <TopMenu />
+
       <TouchableOpacity
         style={styles.content}
         onPress={() => setShowTopMenu(true)}
         onLongPress={() => alert('Long press on content')}
       >
-        <ThemedView style={styles.container}>
-          <Image
-            style={styles.image}
-            source="/assets/images/icon.png"
-            placeholder={{ blurhash }}
-            contentFit="cover"
-            transition={1000}
-          />
-        </ThemedView>
+        <PanGestureHandler onHandlerStateChange={handleSwipe}>
+          <ThemedView style={styles.container}>
+            <Image
+              style={styles.image}
+              source={imageSrc}
+              placeholder={{ blurhash }}
+              contentFit="fill"
+              transition={1000}
+            />
+          </ThemedView>
+        </PanGestureHandler>
       </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  topMenu: {
-    backgroundColor: 'rgba(0, 0, 0, 0.01)',
-    marginVertical: 10,
-    height: 60,
-    borderRadius: 5,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'absolute',
-    top: 20,
-    left: 20,
-    right: 20,
-    zIndex: 2,
-  },
-  topMenuButton: {
-    marginHorizontal: 10,
-    padding: 10,
-  },
   container: {
     position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '100%',
     height: '100%',
     flex: 1,
@@ -63,12 +106,21 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     height: '100%',
+    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
   image: {
     flex: 1,
     width: '100%',
-    backgroundColor: '#0553',
+    backgroundColor: 'white',
+    maxWidth: 400,
+  },
+  ball: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: 'blue',
+    alignSelf: 'center',
   },
 });
