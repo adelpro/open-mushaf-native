14dbb3073e2384458f4345570527136124672223
adelpro <adelpro@gmail.com>
2025-01-04 20:00:07 +0100

    feat(eslint): add accessibility rules and plugin for improved a11y checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier', 'import', 'eslint-plugin-react-compiler'],
+  extends: ['expo', 'prettier', 'plugin:jsx-a11y/recommended'],
+  plugins: ['prettier', 'import', 'eslint-plugin-react-compiler', 'jsx-a11y'],
   rules: {
     'prettier/prettier': 'error',
     'react-compiler/react-compiler': 'error',
+    'jsx-a11y/alt-text': 'warn', // for missing alt attributes in images
+    'jsx-a11y/no-static-element-interactions': 'warn', // for missing role attributes
+    'jsx-a11y/anchor-is-valid': 'warn', // for missing anchor role
     'sort-imports': [
       'error',
       { ignoreCase: true, ignoreDeclarationSort: true },
