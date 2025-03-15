5e2f969f6e69c1527c55d0995af19bd590dbde69
adelpro <adelpro@gmail.com>
2025-03-15 15:31:48 +0100

    revert: revert to the old eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier', 'import', 'eslint-plugin-react-compiler'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-compiler/react-compiler': 'error',
+    'sort-imports': [
+      'error',
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
