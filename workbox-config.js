5f2f8c26bf5dfcffb68222ede5a78951c8ac70e2
adelpro <adelpro@gmail.com>
2025-04-06 14:38:59 +0100

    feat(PWA): add service worker, offline page, and update manifest

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{js,html,png,mp3,ico,json,txt,xml}'],
+  swSrc: 'public/service-worker.js',
   swDest: 'dist/sw.js',
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  maximumFileSizeToCacheInBytes: 104857600,
+  maximumFileSizeToCacheInBytes: 104857600, // 100MB
   clientsClaim: true,
   skipWaiting: true,
 };
