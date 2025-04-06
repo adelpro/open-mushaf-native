e4d7eb6a603fb664ff991bf09fefe4dc2e496d02
adelpro <adelpro@gmail.com>
2025-04-06 14:52:13 +0100

    refactor(workbox): rename swDest and remove unused options

--- workbox-config.js
+++ workbox-config.js
@@ -2,9 +2,6 @@ module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{js,html,png,mp3,ico,json,txt,xml}'],
   swSrc: 'public/service-worker.js',
-  swDest: 'dist/sw.js',
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: 'dist/service-worker.js',
   maximumFileSizeToCacheInBytes: 104857600, // 100MB
-  clientsClaim: true,
-  skipWaiting: true,
 };
