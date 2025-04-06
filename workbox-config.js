bb18812966a9a166ab903bf2ae22f68ea4eaed25
adelpro <adelpro@gmail.com>
2025-04-06 16:17:26 +0100

    refactor(workbox): switch from generateSW to injectManifest for service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{js,html,png,mp3,ico,json,txt,xml}'],
+  swSrc: 'public/service-worker.js',
   swDest: 'dist/service-worker.js',
   maximumFileSizeToCacheInBytes: 104857600, // 100MB
 };
