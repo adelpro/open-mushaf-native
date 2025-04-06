763b14b18fbc707c5a49f00c9542abed4b5813df
adelpro <adelpro@gmail.com>
2025-04-06 16:13:11 +0100

    refactor(workbox): remove swSrc from workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{js,html,png,mp3,ico,json,txt,xml}'],
-  swSrc: 'public/service-worker.js',
   swDest: 'dist/service-worker.js',
   maximumFileSizeToCacheInBytes: 104857600, // 100MB
 };
