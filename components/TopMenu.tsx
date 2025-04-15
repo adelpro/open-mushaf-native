c5458ac0b06d799735122f33e03f24f491c884b5
adelpro <adelpro@gmail.com>
2025-04-15 22:33:03 +0100

    style: adjust TopMenu positioning for better alignment

--- components/TopMenu.tsx
+++ components/TopMenu.tsx
@@ -222,7 +222,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%',
     position: 'absolute',
-    top: 1,
+    top: 0,
     left: 0,
     right: 0,
     backgroundColor: 'transparent',
