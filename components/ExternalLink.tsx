3054bee03a8530b6c0b2d61790f6f7800b663d50
adelpro <adelpro@gmail.com>
2025-04-15 18:09:39 +0100

    refactor(ExternalLink): simplify href handling and type definitions

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,26 +1,22 @@
-import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
-import { Href, Link } from 'expo-router';
+import { Link } from 'expo-router';
 import { openBrowserAsync } from 'expo-web-browser';
 
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & {
-  href: Href<string | object>;
+type Props = Omit<React.ComponentProps<typeof Link>, 'href'> & {
+  href: `http${string}`;
 };
 
 export function ExternalLink({ href, ...rest }: Props) {
-  const hrefString = typeof href === 'string' ? href : href.toString();
   return (
     <Link
       target="_blank"
       {...rest}
-      href={href}
+      href={href as any}
       onPress={async (event) => {
         if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
-          // Open the link in an in-app browser.
-          await openBrowserAsync(hrefString);
+          await openBrowserAsync(href);
         }
       }}
     />
